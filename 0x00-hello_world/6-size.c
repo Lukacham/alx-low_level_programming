5d10fbe9f09d837e64143f5cf431f94e21c8d924
Lukacham <danboyiluka2@gmail.com>
2023-06-15 20:16:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - so we need a program that prints and compile
+ * Return: 0(complete)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	float f;
+	long long int d;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: &lu byte(s)\n" (unsigned long)sizeof(c));
+printf("size of a float int: %lu byte(s)\n" (unsigned long)sizeof(f));
+printf("size of a long long int: $lu byte(S)\n" (unsigned long)sizeof(d));
+return (0);
+}

953246ec2058273306d5c963e0f04008054ec708
Lukacham <danboyiluka2@gmail.com>
2023-06-15 19:07:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Here we go we are asked to finally use printf
+ * Return: 0 (complete)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}

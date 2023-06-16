e627f339400365509565e8196756b5d2a6274de8
Lukacham <danboyiluka2@gmail.com>
2023-06-15 18:50:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The function puts was recommended and not any other function
+ * Return: 0 (Complete)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

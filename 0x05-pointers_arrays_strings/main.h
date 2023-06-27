c9ef6936147c3273b19d9226fca7deb529b4f27f
Lukacham <danboyiluka2@gmail.com>
2023-06-27 02:16:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,4 +11,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strepy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);int _atoi(char *s);
+
+#endif

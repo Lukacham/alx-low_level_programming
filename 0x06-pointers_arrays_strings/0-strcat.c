1111a24148422cb9f372c9a9cf10eeb3d8341e38
Lukacham <danboyiluka2@gmail.com>
2023-06-29 17:01:29 -0700

    hopeful

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int t;
-int j;
-while (dest[t] != '\0')
+	int i;
+	int j;
+
+	i = 0;
+while (dest[i] != '\0')
 {
-t++;
+i++;
 }
 j = 0;
 while (src[j] != '\0')
 {
-dest [t] = src[j];
-t++;
+dest [i] = src[j];
+i++;
 j++;
 }
-dest[t] = '\0';
+dest[i] = '\0';
 return (dest);
 }

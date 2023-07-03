4f34503021577ae6a0fc3f0c3deadb091d154d6e
Lukacham <danboyiluka2@gmail.com>
2023-07-03 15:00:34 -0700

    you can do it

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		dest[k] = src[k];
+
+	return (dest);
+}

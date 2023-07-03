8305cf98e8aa6e3974fd5dfc29bb844bf65acdcf
Lukacham <danboyiluka2@gmail.com>
2023-07-03 15:45:38 -0700

    keep trying

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int k, n, value, check;
+
+	value = 0;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[k])
+			{
+				value++;
+				check = 1;
+			}
+		}
+	}
+	return (check);
+}
